1e69fa49d2c102b4632d9bcbfbb2d0b4a071d0e0
MarriaMarria <maria.kononevs@gmail.com>
2021-04-29 23:20:10 +0200

    updated tuto, added find() to server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 const insertDocument = function(db, callback) {
     db.collection('wednesday_plan').insertOne( { //* collection is a table
 
-        "id" : "2",
+        "id" : "TESTTEST",
         "category" : "personal",
         "name" : "cleaning",
         "description" : "Clean the flat.",
@@ -22,16 +22,29 @@ const insertDocument = function(db, callback) {
     });
 };
 
+const find = function(db, callback) {
+    let cursor = db.collection('wednesday_plan').find();
+        cursor.each(function(err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                console.log(doc);
+            } else {
+                callback();
+            }
+        });
+};
 
 mongoClient.connect(url, function (err, client) {
     assert.equal(null, err);
     console.log('connected');
     const db = client.db('ToDoList'); //! ToDoList is my DB
     insertDocument(db, function() {
-        client.close();
-    });
-    
+        find(db, function() {
+            client.close();
+        }); 
+    }); 
 });
+
 
 //ToDo: to update collection use db.collection('name').updateOne/updateMany({}); to remove use .deleteOne/deleteMany({}); to search in collection use .find()
 // https://docs.microsoft.com/fr-fr/azure/cosmos-db/mongodb-samples
